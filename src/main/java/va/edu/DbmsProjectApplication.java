395e68d2159d77cfa9cde2219218921ca8e56e84
vanh251 <148326521+vanh251@users.noreply.github.com>
2026-04-16 01:10:23 +0700

    first commit

---
+++ src/main/java/va/edu/DbmsProjectApplication.java
@@ -0,0 +1,13 @@
+package va.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbmsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/va/edu/DbmsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package va.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
